2a78cb7fed537714a5c2e947e06586a84e01af23
Fantastycznie <Fantastycznie@192.168.0.101>
2017-07-20 17:30:33 +0200

    .

--- src/Watermark.java
+++ src/Watermark.java
@@ -1,8 +1,44 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
 
 public class Watermark {
-
-	public static void main(String[] args) {
+	/*
+	 * watermarki do zdjec
+	 * wczytaj zdjecie
+	 * dodaj napis
+	 * dodaj obrazek
+	 * zapisz w folderze output
+	 * 
+	 */
+	//public static 
+	public static ImageIO addWatermark(ImageIO img) {
 		
+		
+		
+		
+		ImageIO imgW = null;
+		return imgW;
+	}
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+		BufferedImage img = null;
+		try {
+		    img = ImageIO.read(new File("ith.jpg"));
+		} catch (IOException e) {
+		}
+		
+		System.out.println(img);
+		
+		 
+         
+	       
 	}
 
 }
